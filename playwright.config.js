f96c9474fc01637a1b85a7c38787597fe097646d
itsnotdeon <itsnotdeon@gmail.com>
2026-09-23 09:55:12 +0700

    test: add Playwright Journey verification

---
+++ playwright.config.js
@@ -0,0 +1,2 @@
+const {defineConfig}=require("@playwright/test");
+module.exports=defineConfig({testDir:"./tests/e2e",timeout:30000,use:{baseURL:"http://127.0.0.1:4173/redesign/index.html",trace:"retain-on-failure",screenshot:"only-on-failure"},webServer:{command:"python3 -m http.server 4173 --directory .",url:"http://127.0.0.1:4173/redesign/index.html",reuseExistingServer:true,timeout:10000}});
