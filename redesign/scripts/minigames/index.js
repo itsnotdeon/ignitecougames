9371913090dfd3deacc0810d0ed26ab37d2a8c03
itsnotdeon <itsnotdeon@gmail.com>
2026-09-23 23:43:40 +0700

    fix: refresh minigame dependency modules

--- redesign/scripts/minigames/index.js
+++ redesign/scripts/minigames/index.js
@@ -1,8 +1,8 @@
-import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js?v=20260923-8";
-import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260923-8";
-import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js?v=20260923-8";
-import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260923-8";
-import {saveMemory} from "../features/memories.js?v=20260923-8";
+import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js?v=20260923-10";
+import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260923-10";
+import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js?v=20260923-10";
+import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260923-10";
+import {saveMemory} from "../features/memories.js?v=20260923-10";
 
 const state={active:null,roleplay:createRoleplayState(),king:createKingState(),chess:createChessState(),snake:createSnakeState()};
 
