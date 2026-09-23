4ca372f28e1dadf085de06f6ab6ef4f052ef6406
itsnotdeon <itsnotdeon@gmail.com>
2026-09-23 23:18:34 +0700

    fix: bust cached minigame dependency

--- redesign/scripts/minigames/index.js
+++ redesign/scripts/minigames/index.js
@@ -1,7 +1,7 @@
 import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js";
 import {createKingState,kingAction,renderKing} from "./kingslave.js";
 import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js";
-import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260923-6";
+import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260923-7";
 import {saveMemory} from "../features/memories.js";
 
 const state={active:null,roleplay:createRoleplayState(),king:createKingState(),chess:createChessState(),snake:createSnakeState()};
