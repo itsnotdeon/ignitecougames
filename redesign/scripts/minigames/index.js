c0c19c4addf7ee53211d7f52760534b402382d06
itsnotdeon <itsnotdeon@gmail.com>
2026-09-25 02:13:46 +0700

    Bump King and Slave asset cache versions

--- redesign/scripts/minigames/index.js
+++ redesign/scripts/minigames/index.js
@@ -1,8 +1,8 @@
-import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js?v=20260924-05";
-import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260924-05";
-import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js?v=20260924-05";
-import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260924-05";
-import {saveMemory} from "../features/memories.js?v=20260924-04";
+import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js?v=20260925-15";
+import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260925-15";
+import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js?v=20260925-15";
+import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260925-15";
+import {saveMemory} from "../features/memories.js?v=20260925-15";
 
 const state={active:null,roleplay:createRoleplayState(),king:createKingState(),chess:createChessState(),snake:createSnakeState()};
 
