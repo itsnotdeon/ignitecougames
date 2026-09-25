cd98efa22306ec28b6dc13cea709f0c7d148a413
itsnotdeon <itsnotdeon@gmail.com>
2026-09-26 02:28:45 +0700

    fix: use current King and Slave module cache version

--- redesign/scripts/minigames/index.js
+++ redesign/scripts/minigames/index.js
@@ -1,5 +1,5 @@
 import {createRoleplayState,setRoleplayMode,nextRoleplay,renderRoleplay} from "./roleplay.js?v=20260925-15";
-import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260925-16";
+import {createKingState,kingAction,renderKing} from "./kingslave.js?v=20260926-02";
 import {createChessState,chessClick,chessUndo,renderChess} from "./chess.js?v=20260925-15";
 import {createSnakeState,snakeRoll,snakeContinue,snakeSetMode,snakeReset,renderSnake} from "./snake.js?v=20260925-15";
 import {saveMemory} from "../features/memories.js?v=20260925-15";
