b91416814174ede5f284a924040472322e9c8f7a
itsnotdeon <itsnotdeon@gmail.com>
2026-09-25 23:47:52 +0700

    chore: refresh topic data cache version

--- redesign/scripts/minigames/kingslave.js
+++ redesign/scripts/minigames/kingslave.js
@@ -1,4 +1,4 @@
-import {allKingNormalCommands,allKingDarkCommands} from "../data/topics.js?v=20260924-04";
+import {allKingNormalCommands,allKingDarkCommands} from "../data/topics.js?v=20260925-19";
 
 const NORMAL = allKingNormalCommands;
 const DARK = allKingDarkCommands;
