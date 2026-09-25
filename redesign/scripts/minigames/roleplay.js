17e1672a28a6b92e738c112e38b27993f6f669b1
itsnotdeon <itsnotdeon@gmail.com>
2026-09-26 02:09:48 +0700

    Bump content module cache version

--- redesign/scripts/minigames/roleplay.js
+++ redesign/scripts/minigames/roleplay.js
@@ -1,4 +1,4 @@
-import {allRoleplayNormalRoles,allRoleplayDarkRoles,roleplayCustomItems,roleplayCustomItemsExplicit} from "../data/topics.js?v=20260925-20";
+import {allRoleplayNormalRoles,allRoleplayDarkRoles,roleplayCustomItems,roleplayCustomItemsExplicit} from "../data/topics.js?v=20260926-01";
 
 function customRolePool(items){
   const groups=new Map();
