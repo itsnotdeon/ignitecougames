108a0304c1f7a2f30160a2e190604ec74b97aef6
itsnotdeon <itsnotdeon@gmail.com>
2026-09-27 01:58:21 +0700

    Bust UI module caches after UX fixes

--- redesign/scripts/features/ui.js
+++ redesign/scripts/features/ui.js
@@ -1,11 +1,11 @@
 import {getMemories,getMemoryStats} from "./memories.js?v=20260925-18";
-import {getPreferences,savePreferences,VIBES} from "./preferences.js?v=20260925-18";
+import {getPreferences,savePreferences,VIBES} from "./preferences.js?v=20260927-03";
 import {getUnlocks} from "./unlocks.js?v=20260925-18";
-import {createSurpriseContext} from "./surprise.js?v=20260925-18";
-import {buildJourney} from "./dynamicJourney.js?v=20260926-01";
+import {createSurpriseContext} from "./surprise.js?v=20260927-03";
+import {buildJourney} from "./dynamicJourney.js?v=20260927-03";
 import {getAdaptive} from "./adaptive.js?v=20260924-05";
 import {getProgress,achievementList} from "../progression.js?v=20260925-16";
-import {buildContext,getMoodMeta,MOODS,setMood} from "./context.js?v=20260925-18";
+import {buildContext,getMoodMeta,MOODS,setMood} from "./context.js?v=20260927-03";
 function esc(v){return String(v).replace(/[&<>"']/g,c=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#039;"}[c]))}
 export function getCurrentContext(level){return buildContext({level,preferences:getPreferences(),memories:getMemories()})}
 function formatTime(hour,minute){return String(hour).padStart(2,"0")+":"+String(minute).padStart(2,"0")}
